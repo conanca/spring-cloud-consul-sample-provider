e5f70493e9948687b5a0fd3a9376179381ca64d1
gongchengdong <gongchengdong@news.cn>
2017-02-15 17:01:59 +0800

    增加RefreshScope朱姐

--- src/main/java/org/springframework/cloud/consul/sample/controller/HelloController.java
+++ src/main/java/org/springframework/cloud/consul/sample/controller/HelloController.java
@@ -1,5 +1,6 @@
 package org.springframework.cloud.consul.sample.controller;
 
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Created by conanca on 17-2-15.
  */
+@RefreshScope
 @RestController
 public class HelloController {
     @RequestMapping(value = "/hello/{name}", method = RequestMethod.GET)
