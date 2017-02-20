096612f4f0906f9afcee562214350f6db4503d57
gongchengdong <gongchengdong@news.cn>
2017-02-20 16:25:04 +0800

    演示断路器控制调用超时

--- src/main/java/org/springframework/cloud/consul/sample/controller/HelloController.java
+++ src/main/java/org/springframework/cloud/consul/sample/controller/HelloController.java
@@ -1,10 +1,15 @@
 package org.springframework.cloud.consul.sample.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
 
 /**
  * Created by conanca on 17-2-15.
@@ -12,8 +17,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 @RestController
 public class HelloController {
+
     @RequestMapping(value = "/hello/{name}", method = RequestMethod.GET)
     public String sayHello(@PathVariable(value = "name") String name) {
         return "Hello " + name + "!";
+    }
+
+    @RequestMapping("hello2")
+    public String sayHello2() {
+        long start = System.currentTimeMillis();
+        // 随机睡眠1000毫秒以内
+        try {
+            Thread.sleep(new Random().nextInt(1000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        long cost = System.currentTimeMillis() - start;
+        return "Hello~ " + ", spent " + cost;
     }
 }
