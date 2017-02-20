6339e0d9c163073f71ffb3ad9aea7687b930fba5
gongchengdong <gongchengdong@news.cn>
2017-02-20 17:14:27 +0800

    移除多余内容

--- src/main/java/org/springframework/cloud/consul/sample/controller/HelloController.java
+++ src/main/java/org/springframework/cloud/consul/sample/controller/HelloController.java
@@ -1,8 +1,5 @@
 package org.springframework.cloud.consul.sample.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
